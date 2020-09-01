2fad5f5fd4b97f2516a4882031f6cb274c1d2318
branislav94 <branislav94@live.com>
2020-09-02 00:57:02 +0200

    Timeline Updated

--- layouts/TopMenu.js
+++ layouts/TopMenu.js
@@ -11,7 +11,7 @@ const TopMenu = () => {
   return (
     <Menu theme="light" mode="horizontal" defaultSelectedKeys={['1']}>
       {/* <Menu.Item key="3"><SearchOutlined /></Menu.Item> */}
-      <Menu.Item key="2"><QuestionCircleOutlined /> <Link href="/progress"><a></a></Link></Menu.Item>
+      <Menu.Item key="2"><QuestionCircleOutlined style={{ fontSize: '18px' }}  /> <Link href="/progress"><a></a></Link></Menu.Item>
       {/* <Menu.Item key="1"><BellOutlined />
         <Badge count={99} overflowCount={10}>
           <a href="#" className="head-example" />
